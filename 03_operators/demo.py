c5b3194410d19605bbd7cc028cb2c2b4268f0174
ravi2krishna <ravi2krishna@gmail.com>
2026-09-04 11:48:42 +0530

    Class - 6 - Operators - Logical - Membership - Identity - Bitwise - Data Types

--- 03_operators/demo.py
+++ 03_operators/demo.py
@@ -53,3 +53,72 @@ print(num1 == num2)
 print(num1 != num2)
 print(num1 > num2)
 print(num1 < num2)
+
+print("=================")
+
+# Logical Operators 
+num1 = 4
+num2 = 3
+num3 = 2
+num4 = 1
+
+print(num1 > num2 and num3 > num4) # T and T -> T
+print(num1 > num2 and num3 < num4) # T and F -> F
+
+print(num1 > num2 or num3 > num4) # T or T -> T
+print(num1 > num2 or num3 < num4) # T or F -> T
+
+print(num1 > num2) # T 
+print(not num1 > num2) # T 
+
+print("=================")
+
+# Membership Operators 
+data = "python is an programming language"
+find_word = "java"
+status = find_word in data
+print(status)
+
+data = "python is an programming language"
+find_word = "python"
+status = find_word in data
+print(status)
+
+data = "python is an programming language"
+find_word = "java"
+status = find_word not in data
+print(status)
+
+# List Data Type -> Complex Data Types Used To Store Multiple Values, Represented Using []
+list_employee_ids = [101,102,103,105,108,112,120]
+find_emp_by_id = 120
+status = find_emp_by_id in list_employee_ids
+print("Employee Found: ",status)
+print("Employee Found: ",104 in list_employee_ids)
+
+print("=================")
+
+# Identity Operators 
+
+value_x = 10 # 1234
+print(id(value_x))
+
+value_y = 100 # 4567
+print(id(value_y))
+
+value_z = 10 # 1234
+print(id(value_z))
+
+print(value_x is value_y)
+print(value_x is value_z)
+
+print("=================")
+
+# Bitwise Operators 
+n1 = 5 # 0000000000000101
+n2 = 3 # 0000000000000011
+       # 0000000000000111
+       # 0000000000000001
+
+print(n1 & n2) # 1 
+print(n1 | n2) # 7
---
+++ 04_data_types/demo.py
@@ -0,0 +1,52 @@
+# Data Types
+
+# Numeric Types 
+
+data = 10 
+print(type(data))
+
+data = -10 
+print(type(data))
+
+data = 10.5 
+print(type(data))
+
+data = -10.5 
+print(type(data))
+
+# complex number -> a + ib 
+# data = 3 + i5 # Error
+# print(type(data))
+
+data = 3 + 5j # No Error
+print(type(data))
+
+data = True
+print(type(data))
+
+data = False
+print(type(data))
+
+data = None
+print(type(data))
+
+data = "hello there"
+print(type(data))
+
+# Complex Data Types 
+
+# Lists
+data = [10,20,30,40,50]
+print(type(data))
+
+# Tuples
+data = (10,20,30,40,50)
+print(type(data))
+
+# Sets
+data = {10,20,30,40,50}
+print(type(data))
+
+# Dictionaries
+data = {"course":"python","timing":10,"location":"hyderabad"}
+print(type(data))
